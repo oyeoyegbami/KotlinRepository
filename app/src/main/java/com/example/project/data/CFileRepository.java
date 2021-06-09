7114e443ce8a185bd03e6d7ea79a6b1f05c2dfda
Israel_Ayoola <israelayoola09@gmail.com>
2021-06-09 23:31:13 +0100

    BackBlaze

---
+++ app/src/main/java/com/example/project/data/CFileRepository.java
@@ -0,0 +1,70 @@
+package com.example.project.data;
+
+import android.app.Application;
+import androidx.lifecycle.LiveData;
+import java.util.List;
+
+/**
+ * Abstracted Repository as promoted by the Architecture Guide.
+ * https://developer.android.com/topic/libraries/architecture/guide.html
+ */
+public class CFileRepository {
+
+    private CFileDao mCFileDao;
+    private LiveData<List<CFile>> mAllCFiles;
+    /**
+     * Note : In order to unit test the CFileRepository, you have to remove the Application
+     * dependency. This adds complexity and much more code, and this sample is not about testing.
+     * @param application constructor accepts application
+     */
+    public CFileRepository(Application application) {
+        CFileRoomDatabase db = CFileRoomDatabase.getDatabase(application);
+        mCFileDao = db.wordDao();
+        mAllCFiles = mCFileDao.getSortedFilesCounts();
+    }
+
+    /**
+     * Room executes all queries on a separate thread.
+     * Observed LiveData will notify the observer when the data has changed.
+     * @return a liveData list of files
+     */
+    public LiveData<List<CFile>> getAllCFiles() {
+        return mAllCFiles;
+    }
+
+    /**
+     * You must call this on a non-UI thread or your app will throw an exception. Room ensures
+     * that you're not doing any long running operations on the main thread, blocking the UI.
+     * @param cfile an object containing a file and the count
+     * You must call this on a non-UI thread or your app will throw an exception. Room ensures
+     * that you're not doing any long running operations on the main thread, blocking the UI.
+     */
+    public void insert(CFile cfile) {
+        CFileRoomDatabase.databaseWriteExecutor.execute(() -> {
+            mCFileDao.insert(cfile);
+        });
+    }
+
+    /**
+     * You must call this on a non-UI thread or your app will throw an exception. Room ensures
+     * that you're not doing any long running operations on the main thread, blocking the UI.
+     */
+    public void deleteAll() {
+        CFileRoomDatabase.databaseWriteExecutor.execute(() -> {
+            mCFileDao.deleteAll();
+        });
+    }
+
+    /**
+     * You must call this on a non-UI thread or your app will throw an exception. Room ensures
+     * that you're not doing any long running operations on the main thread, blocking the UI.
+     */
+    public void reProcessFilesAgain() {
+        CFileRoomDatabase.databaseWriteExecutor.execute(() -> {
+            CFileRoomDatabase.executePullFilesRequest();
+        });
+    }
+
+
+    
+}
---
+++ app/src/main/java/com/example/project/views/CFileListAdapter.java
@@ -0,0 +1,88 @@
+package com.example.project.views;
+
+import android.content.Context;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.example.project.R;
+import com.example.project.data.CFile;
+
+import java.util.List;
+
+
+public class CFileListAdapter extends RecyclerView.Adapter<CFileListAdapter.FileViewHolder> {
+
+    class FileViewHolder extends RecyclerView.ViewHolder {
+        private final TextView fileItemView;
+        private final TextView countItemView;
+
+        private FileViewHolder(View itemView) {
+            super(itemView);
+            fileItemView = itemView.findViewById(R.id.textView);
+            countItemView = itemView.findViewById(R.id.countView);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<CFile> mFiles; // Cached copy of words
+    private int lastPosition = -1;
+    Context context;
+
+    CFileListAdapter(Context context) {
+        this.context = context;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public FileViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview_item, parent, false);
+        return new FileViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(FileViewHolder holder, int position) {
+        if (mFiles != null) {
+            CFile current = mFiles.get(position);
+            holder.fileItemView.setText(current.getFilename());
+            holder.countItemView.setText(current.getCount()+"");
+        } else {
+            // Covers the case of data not being ready yet.
+            holder.fileItemView.setText(context.getString( R.string.no_text_file));
+        }
+
+        setAnimation(holder.itemView, position);
+    }
+
+    private void setAnimation(View viewToAnimate, int position)
+    {
+        // If the bound view wasn't previously displayed on screen, it's animated
+        if (position > lastPosition)
+        {
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.push_down_in);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
+    }
+
+    void setFileInfo(List<CFile> cFileList) {
+        mFiles = cFileList;
+        notifyDataSetChanged();
+    }
+
+    // getItemCount() is called many times, and when it is first called,
+    // mWords has not been updated (means initially, it's null, and we can't return null).
+    @Override
+    public int getItemCount() {
+        if (mFiles != null)
+            return mFiles.size();
+        else return 0;
+    }
+
+}
+
+
